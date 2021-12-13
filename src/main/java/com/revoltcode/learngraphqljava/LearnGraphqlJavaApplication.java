d6766066707ce6b128d02f7838bede6f781220ca
Revolt <revolt@localhost.localdomain>
2021-12-13 15:42:36 +0100

    first commit

---
+++ src/main/java/com/revoltcode/learngraphqljava/LearnGraphqlJavaApplication.java
@@ -0,0 +1,13 @@
+package com.revoltcode.learngraphqljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGraphqlJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGraphqlJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revoltcode/learngraphqljava/LearnGraphqlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.learngraphqljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
